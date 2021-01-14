40e12264b73c359813a8b0be8737cd6bce32765a
Jeremie-OP <jeremie.opigez@alumni.univ-avignon.fr>
2021-01-14 14:58:02 +0100

    ajout des view error/non de login et signup

---
+++ wiwiCar/view/loginError.php
@@ -0,0 +1,2 @@
+<meta http-equiv="refresh" content="5;wiwiCar.php"/> <!-- 5 sec delay-->
+<div align="center" id="error" class="container text-white">Erreur d'identifiant<br>Redirection dans 5 secondes...</div>
---
+++ wiwiCar/view/signupNone.php
@@ -0,0 +1,2 @@
+<meta http-equiv="refresh" content="5;wiwiCar.php"/> <!-- 5 sec delay-->
+<div align="center" id="succes" class="container text-white">Création du compte abandonnée, l'identifiant existe deja<br>Redirection dans 5 secondes...</div>
